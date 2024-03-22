a1057c78b5921d083dc7b6ee85bfa024279b905b
IsacOjeda <isacojeda717@gmail.com>
2024-03-22 15:47:30 -0700

    Donut in C
    3D donut in C, doesnt work as of now but i'll get back to it

--- heart.py
+++ heart.py
@@ -8,7 +8,7 @@ def heartb(k):
     math.cos(3*k)-\
     math.cos(4*k)
 speed(1000)
-bgcolor("black")
+bgcolor("pink")
 for i in range(6000):
     goto(hearta(i)*20,heartb(i)*20)
     for j in range(5):
